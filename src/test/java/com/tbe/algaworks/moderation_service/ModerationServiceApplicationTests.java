61c5824007bd3b4e7e776846226de838cb0bd5a1
thiago <thiago.bertachini@hotmail.com>
2025-07-07 12:16:51 -0300

    initial commit

---
+++ src/test/java/com/tbe/algaworks/moderation_service/ModerationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbe.algaworks.moderation_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
